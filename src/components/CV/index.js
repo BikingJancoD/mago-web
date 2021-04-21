859d004ac2edd27dc3713de07514ee1affe15f90
BikingJancoD <82882708+BikingJancoD@users.noreply.github.com>
2021-04-20 20:07:29 -0500

    Update index.js

--- src/components/CV/index.js
+++ src/components/CV/index.js
@@ -2,13 +2,11 @@ import React from 'react'
 import { Layout } from 'antd'
 import Header from './Header'
 import Sidebar from '../Sidebar'
-import TopTech from './TopTech'
 import SkillList from './SkillList'
 import Experience from './Experience'
 import PitchLine from './PitchLine'
 import Volunteer from './Volunteer'
 import Education from './Education'
-import Experiments from './Experiments'
 import Recommendations from './Recommendations'
 import Footer from './Footer'
 import data from './data.json'
@@ -18,12 +16,8 @@ const CV = () => (
   <>
     <Sidebar hideMobile={true} />
     <Content className="m-5">
-      {data.header && <Header header={data.header} />}
-      {data.experiments && (
-        <Experiments experiments={data.experiments} tech={data.tech} />
-      )}
-      {data.pitchLine && <PitchLine pitchLine={data.pitchLine} />}
-      {data.tech && data.topTech && <TopTech topTech={data.topTech} tech={data.tech} />}
+      {data.header && <Header header={data.header} />}      
+      {data.pitchLine && <PitchLine pitchLine={data.pitchLine} />}      
       {data.skills && <SkillList skills={data.skills} />}
       {data.experience && <Experience experience={data.experience} />}
       {data.volunteer && <Volunteer volunteer={data.volunteer} />}
